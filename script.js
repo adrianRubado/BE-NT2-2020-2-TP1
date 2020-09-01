276df38e6ca9be9d1a4293aa8a05327bd2b9140f
Adrian Ariel Rubado <aarubado@baufest.com>
2020-09-01 13:30:27 -0300

    agregar tarea y contadores

--- script.js
+++ script.js
@@ -9,6 +9,56 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+
+
+
+
 function addTodo() {
-  alert('Boton Add TODO clickeado!')
+  //alert('Boton Add TODO clickeado!')
+  list.appendChild(createMenuItem()) 
+  
+  
+  
+    }
+
+  
+
+function createMenuItem(name) {
+var count = parseInt(itemCountSpan.innerText)  
+count+= 1
+var ul= document.createElement("ul");
+var description = document.createTextNode("Tarea "+ count);
+var checkbox = document.createElement("input");
+
+checkbox.type = "checkbox";    
+checkbox.name = "slct[]";      
+checkbox.value = name;
+checkbox.id = name
+checkbox.onclick = function() {onClicked(this)}
+       
+
+ul.appendChild(checkbox);   
+ul.appendChild(description)
+
+itemCountSpan.innerText = count; 
+
+uncheckedCountSpan.innerText = parseInt(uncheckedCountSpan.innerText) + 1;
+
+
+
+
+return ul
 }
+
+
+function onClicked(checkbox) { 
+  if (checkbox.checked == true) {
+    uncheckedCountSpan.innerText = parseInt(uncheckedCountSpan.innerText) - 1; 
+  } else {
+    uncheckedCountSpan.innerText = parseInt(uncheckedCountSpan.innerText) + 1; 
+  }
+}
+
+
+
+
